f9a84991ce1e6373b5810fbb37c86e83a635d81d
RoryJulian <x22171983@student.ncirl.ie>
2023-12-11 19:24:55 +0000

    Newsfeed update

--- NewsFeedService/client/app.js
+++ NewsFeedService/client/app.js
@@ -7,7 +7,7 @@ var packageDefinition = protoLoader.loadSync(PROTO_PATH)
 var newsfeed_proto = grpc.loadPackageDefinition(packageDefinition).newsfeed
 var client = new newsfeed_proto.NewsfeedService("0.0.0.0:40000", grpc.credentials.createInsecure());
 
-var amount = parseInt(readlineSync.question("Indicate using a number the information that would be most helpful to you\n\n 1. Postcodes containing roadworks\n 2. Postcodes containing tolled roads \n 3. Postcodes with high traffic levels\n \n"))
+var amount = parseInt(readlineSync.question("Indicate using a number the information that would be most helpful to you\n\n 1. Postcodes containing roadworks/diversions\n 2. Postcodes containing tolled roads \n 3. Postcodes with high current traffic levels\n \n"))
 var call = client.generateNewsfeed({amount: amount})
 
 call.on('data', function(response) {
