7018f1dfe387d086a19d2f9ab5b6e5c2390a219a
RoryJulian <x22171983@student.ncirl.ie>
2023-12-15 10:28:11 +0000

    Functionality update

--- ThreadService/client/app.js
+++ ThreadService/client/app.js
@@ -9,16 +9,16 @@ var client = new thread_proto.ThreadService("0.0.0.0:40000", grpc.credentials.cr
 
 var call = client.calculateThread(function(error, response){
     if(error){
-        console.log("An error occured")
+        console.log("An error occured, it looks like you are no longer connected to the server")
     } else {
-        console.log("The mean is " + response.mean)
+        console.log("We estimate that this journey on the M50 will take you " + response.mean + " minutes. Thank you for using the M50 Traffic Planner.")
     }
 })
 
 var user_input 
-
+console.log("Welcome to the M50 Traffic Planner. Please enter the duration of your most recent trips (in minutes) on the M50 and we will collate this our realtime data to provide an accurate journey duration estimate!\n")
 while(true){
-    user_input = readlineSync.question("Enter a number (q to Quit):")
+    user_input = readlineSync.question("Please enter previous journey duration in minutes (q to Quit):\n\n")
     if(user_input.toLowerCase() === "q") {
         break
     }
