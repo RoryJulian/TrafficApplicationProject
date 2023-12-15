d97fd287a82759cc2aa366618fdf58c86ef11ae0
RoryJulian <x22171983@student.ncirl.ie>
2023-12-15 15:52:26 +0000

    Updated Unary

--- PostService/client/app.js
+++ PostService/client/app.js
@@ -7,16 +7,21 @@ var packageDefinition = protoLoader.loadSync(PROTO_PATH)
 var post_proto = grpc.loadPackageDefinition(packageDefinition).post
 var client = new post_proto.PostService("0.0.0.0:40000", grpc.credentials.createInsecure());
 
-var number1 = readlineSync.question("Please indicate your update type.\n1. High Traffic\n2. Roadworks/Diversions\n\n")
-var number2 = readlineSync.question("What is the postcode you are currently in? (i.e. Dublin ___?)\n\n")
+console.log("Welcome to the Trip Cost Calculator. Please input the below required details and we will confirm the total cost of your journey.\n")
+var number1 = readlineSync.question("How much in Euro did you spend on tolled roads?\n\n")
+var number2 = readlineSync.question("How much in Euro did you spend on Parking?\n\n")
 
 try{
     client.add({number1: number1, number2: number2}, function(error, response) {
+       try{
         if(response.message){
             console.log(response.message)
         }else {
-            console.log(response.result)
+            console.log("The total amount spent on your journey came to " + response.result + " Euro.")
         }
+       } catch(e) {
+        console.log("Could not connect to server")
+       }
     })
 
 } catch(e) {
--- PostService/server/app.js
+++ PostService/server/app.js
@@ -7,25 +7,13 @@ var packageDefinition = protoLoader.loadSync(
 var post_proto = grpc.loadPackageDefinition(packageDefinition).post
 
 function add(call, callback){
-    try{
-        var number1 = parseInt(call.request.number1)
+    var number1 = parseInt(call.request.number1)
         var number2 = parseInt(call.request.number2)
-        if(!isNaN(number1) && !isNan(number2)){
-            var result = number1 + number2
+        var result = number1 + number2
             callback(null, {
                 message: undefined,
-                result: result
-            })
-        } else{
-            callback(null, {
-                message: "Please specify two numbers"
-            })
-        }
-    } catch(e) {
-        callback(null, {
-            message: "Thank you for submitting, your post was successful."
-        })
-    }
+                result: result 
+    }) 
 }
 
 var server = new grpc.Server()
@@ -33,8 +21,3 @@ server.addService(post_proto.PostService.service, {add: add})
 server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function(){
     server.start()
 })
-
-
-
-
-
