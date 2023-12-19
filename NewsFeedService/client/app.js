9def9963df96f6bb338fb68e3dc21e49132cd136
RoryJulian <x22171983@student.ncirl.ie>
2023-12-19 18:45:03 +0000

    Welcome update

--- NewsFeedService/client/app.js
+++ NewsFeedService/client/app.js
@@ -8,6 +8,7 @@ var newsfeed_proto = grpc.loadPackageDefinition(packageDefinition).newsfeed
 var client = new newsfeed_proto.NewsfeedService("0.0.0.0:40000", grpc.credentials.createInsecure());
 
 //Welcome message and prompt for user to select information that is relevant for them 
+console.log("Welcome to the Smart Traffic Newsfeed, where you can get all the updates you need to arrive safe and on time!")
 var amount = parseInt(readlineSync.question("Indicate using a number the information that would be most helpful to you\n\n 1. Postcodes containing roadworks/diversions\n 2. Postcodes containing tolled roads \n 3. Postcodes with high current traffic levels\n \n"))
 var call = client.generateNewsfeed({amount: amount})
 
